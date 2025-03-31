a7c71fd6c53f54b78ef6e2c9913d213b85a09d42
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-03-31 19:50:34 -0400

    Move select folder functions So that they can access label variables

--- Main.py
+++ Main.py
@@ -15,55 +15,46 @@ def load_config():
 
 
 def select_folder():
-    folderPath = filedialog.askdirectory("Select a folder")
+    folderPath = filedialog.askdirectory(title="Select a folder")
     if folderPath:
         return folderPath
     else:
         return None
     
-def select_world_folder(world_label):
-    folderPath = select_folder()
-    if folderPath:
-        CONFIG_FILE["worlds_folder"] = folderPath
-        world_label.config(text=folderPath)
-
-def select_backup_folder(backup_label):
-    folderPath = select_folder()
-    if folderPath:
-        CONFIG_FILE["backup_folder"] = folderPath
-        backup_label.config(text=folderPath)
-
-def LoadDirectory(dirName):
-    try:
-        with open(CONFIG_FILE, "r") as file:
-            for line in file:
-                data = json.loads(line.strip())
-                print(data)
-            return None
-    except FileNotFoundError:
-        return None    
-    
 def SetupGUI(config):
+
+    def select_world_folder():
+        folderPath = select_folder()
+        if folderPath:
+            CONFIG_FILE["worlds_folder"] = folderPath
+            worlds_label.config(text=folderPath)
+
+    def select_backup_folder():
+        folderPath = select_folder()
+        if folderPath:
+            CONFIG_FILE["backup_folder"] = folderPath
+            backup_label.config(text=folderPath)
+
     root = tk.Tk()
     root.title("Bedrock Backup")
-
-    button = tk.Button(root, text = "Select Minecraft World Folder", command = select_world_folder(worlds_label))
-    button.grid(row = 0, column = 0, padx = 10, pady = 10)
 
     worlds_label = tk.Label(root, text=f"Worlds Folder: {config['worlds_folder']}")
     worlds_label.grid(row = 0, column = 1, padx = 10, pady = 10)
 
-    button = tk.Button(root, text = "Select Backup Folder", command = select_backup_folder(backup_label))
-    button.grid(row = 1, column = 0, padx = 10, pady = 10)
+    button = tk.Button(root, text = "Select Minecraft World Folder", command = select_world_folder)
+    button.grid(row = 0, column = 0, padx = 10, pady = 10)
 
     backup_label = tk.Label(root, text=f"Backup Folder: {config['backup_folder']}")
     backup_label.grid(row = 1, column = 1, padx = 10, pady = 10)
 
+    button = tk.Button(root, text = "Select Backup Folder", command = select_backup_folder)
+    button.grid(row = 1, column = 0, padx = 10, pady = 10)
+
     root.mainloop()
 
 def Initialize():
-
-    SetupGUI()
+    config = load_config()
+    SetupGUI(config)
 
 
 Initialize()
